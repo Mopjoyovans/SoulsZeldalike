dc386e7af48904d02d9d3fc8edeef6740bcc5e73
Mopjoyovans <mattstankey@gmail.com>
2023-10-12 21:06:48 -0500

    Enemy death animation and refactor

--- scenes/enemy/bat.gd
+++ scenes/enemy/bat.gd
@@ -1,8 +1,11 @@
 extends CharacterBody2D
 
+const enemy_death_effect_scene = preload("res://scenes/effects/enemy_death_effect.tscn")
+
 @onready var stats_component: StatsComponent = $StatsComponent
 
 @export var knockback = 130.0
+
 var knockback_speed = 200.0
 
 
@@ -17,4 +20,7 @@ func _on_hurtbox_component_area_entered(area):
 
 
 func _on_stats_component_died():
+	var enemy_death_effect = enemy_death_effect_scene.instantiate()
+	get_parent().add_child(enemy_death_effect)
+	enemy_death_effect.global_position = global_position
 	queue_free()
--- scenes/object/grass.gd
+++ scenes/object/grass.gd
@@ -1,11 +1,10 @@
 extends Node2D
 
+const grass_effect_scene = preload("res://scenes/effects/grass_effect.tscn")
 
 func create_grass_effect():
-	var grass_effect_scene: PackedScene = load("res://scenes/effects/grass_effect.tscn")
 	var grass_effect = grass_effect_scene.instantiate()
-	var world = get_tree().current_scene
-	world.add_child(grass_effect)
+	get_parent().add_child(grass_effect)
 	grass_effect.global_position = global_position
 
 
